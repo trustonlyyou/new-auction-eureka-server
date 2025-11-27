814c8a1d90ca95e38e86a2634e6774d72be476c6
trushonlyyou <trushonlyyou@tnctec.co.kr>
2025-11-28 00:43:21 +0900

    - init project

---
+++ src/main/java/com/lunghwan/auction/NewAuctionEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.lunghwan.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAuctionEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewAuctionEurekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lunghwan/auction/NewAuctionEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunghwan.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAuctionEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
